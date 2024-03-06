3c2dc8085e0404ed379366de6d8e8bc5cc3a8c62
Ali <lisalmanov84@gmail.com>
2024-03-06 22:55:01 +0400

    I finished some of the CRUD operations

---
+++ Chrono/Areas/Admin/Controllers/MaterialsController.cs
@@ -0,0 +1,159 @@
+﻿using Chrono.DAL;
+using Chrono.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chrono.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MaterialsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MaterialsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Materials
+        public async Task<IActionResult> Index()
+        {
+            return _context.Materials != null ?
+                        View(await _context.Materials.ToListAsync()) :
+                        Problem("Entity set 'AppDbContext.Materials'  is null.");
+        }
+
+        // GET: Admin/Materials/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Materials == null)
+            {
+                return NotFound();
+            }
+
+            var material = await _context.Materials
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (material == null)
+            {
+                return NotFound();
+            }
+
+            return View(material);
+        }
+
+        // GET: Admin/Materials/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Materials/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Id")] Material material)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(material);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(material);
+        }
+
+        // GET: Admin/Materials/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Materials == null)
+            {
+                return NotFound();
+            }
+
+            var material = await _context.Materials.FindAsync(id);
+            if (material == null)
+            {
+                return NotFound();
+            }
+            return View(material);
+        }
+
+        // POST: Admin/Materials/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Name,Id")] Material material)
+        {
+            if (id != material.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(material);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MaterialExists(material.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(material);
+        }
+
+        // GET: Admin/Materials/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Materials == null)
+            {
+                return NotFound();
+            }
+
+            var material = await _context.Materials
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (material == null)
+            {
+                return NotFound();
+            }
+
+            return View(material);
+        }
+
+        // POST: Admin/Materials/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Materials == null)
+            {
+                return Problem("Entity set 'AppDbContext.Materials'  is null.");
+            }
+            var material = await _context.Materials.FindAsync(id);
+            if (material != null)
+            {
+                _context.Materials.Remove(material);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MaterialExists(int id)
+        {
+            return (_context.Materials?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Chrono/Areas/Admin/Controllers/RatingsController.cs
@@ -0,0 +1,159 @@
+﻿using Chrono.DAL;
+using Chrono.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chrono.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RatingsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public RatingsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Ratings
+        public async Task<IActionResult> Index()
+        {
+            return _context.Ratings != null ?
+                        View(await _context.Ratings.ToListAsync()) :
+                        Problem("Entity set 'AppDbContext.Ratings'  is null.");
+        }
+
+        // GET: Admin/Ratings/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Ratings == null)
+            {
+                return NotFound();
+            }
+
+            var rating = await _context.Ratings
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (rating == null)
+            {
+                return NotFound();
+            }
+
+            return View(rating);
+        }
+
+        // GET: Admin/Ratings/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Ratings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Vote,ProductId,Id")] Rating rating)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(rating);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rating);
+        }
+
+        // GET: Admin/Ratings/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Ratings == null)
+            {
+                return NotFound();
+            }
+
+            var rating = await _context.Ratings.FindAsync(id);
+            if (rating == null)
+            {
+                return NotFound();
+            }
+            return View(rating);
+        }
+
+        // POST: Admin/Ratings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Vote,ProductId,Id")] Rating rating)
+        {
+            if (id != rating.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(rating);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RatingExists(rating.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rating);
+        }
+
+        // GET: Admin/Ratings/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Ratings == null)
+            {
+                return NotFound();
+            }
+
+            var rating = await _context.Ratings
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (rating == null)
+            {
+                return NotFound();
+            }
+
+            return View(rating);
+        }
+
+        // POST: Admin/Ratings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Ratings == null)
+            {
+                return Problem("Entity set 'AppDbContext.Ratings'  is null.");
+            }
+            var rating = await _context.Ratings.FindAsync(id);
+            if (rating != null)
+            {
+                _context.Ratings.Remove(rating);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RatingExists(int id)
+        {
+            return (_context.Ratings?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
